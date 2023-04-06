8f17c20698f1586be09ce6547f941cabb27640c3
kalpanarathod-me <121625344+kalpanarathod-me@users.noreply.github.com>
2023-04-06 15:15:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngStarter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-j75h9d)
---
+++ src/app/example.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: 'example.component.html',
+})
+export class ExampleComponent {
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete'
+      }
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share'
+      }
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel'
+      }
+    }
+  ];
+}
